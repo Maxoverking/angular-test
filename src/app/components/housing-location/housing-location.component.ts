acc57678c304a6e4caefb55d720150bb7062b719
Maxoverking <avemax7@gmail.com>
2023-11-01 22:54:48 +0100

    add homes

--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from 'src/app/models/housingLocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.scss']
 })
 export class HousingLocationComponent {
+  constructor() { }
+  @Input() housingLocation!: HousingLocation
+
 
 }
